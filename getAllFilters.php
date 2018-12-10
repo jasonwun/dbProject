188f2736df12bd3a80d6de5ddb6123ccefc0d09f
JUNDA YIN <yinjunda.cool@gmail.com>
2018-12-10 05:51:54 -0500

    APIs for filters

---
+++ getAllFilters.php
@@ -0,0 +1,33 @@
+<?php
+require("db.inc.php");
+
+$uid = $_GET["uid"];
+
+$pdo = pdo_connect();
+
+$dom = new DOMDocument("1.0");
+
+$node = $dom->createElement("filter");
+$parnode = $dom->appendChild($node);
+
+$query = sprintf("select * from filter where fuid = '%s'",
+$uid);
+
+$result = $pdo->query($query);
+
+if($result->rowCount() == 0){
+    header("HTTP/1.0 404 Not Found");
+    exit();
+}
+
+header("Content-type: text/xml");
+// Iterate through the rows, adding XML nodes for each
+while ($row = $result->fetch()){
+  $node = $dom->createElement("filter");
+  $newnode = $parnode->appendChild($node);
+  $newnode->setAttribute("filterid", $row['fid']);
+  $newnode->setAttribute("filterdesc", $row["fname"]);
+}
+echo $dom->saveXML();
+
+?>

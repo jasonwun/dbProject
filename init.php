f496fd6f56c6b742a6eb1832c072293324ee1dbd
JUNDA YIN <yinjunda.cool@gmail.com>
2018-12-12 16:08:46 -0500

    Quite A lot of things to change

--- init.php
+++ init.php
@@ -24,24 +24,20 @@
 
   <?php
   session_start();
-  /*echo $_SESSION['username'];*/
+  $username =  $_SESSION['username'];
+  $uid = $_SESSION['uid'];
+  $ulat = $_SESSION['ulat'];
+  $ulng = $_SESSION['ulng'];
+  $utime = $_SESSION['utime'];
+  $ustate = $_SESSION['ustate'];
   ?>
-  <body style="margin:0px; padding:0px;" onload="initMap()">
-
+  <body style="margin:0px; padding:0px;">
+    
     <div id="map" style="width: 50%; height: 100%; float:left"></div>
 
-    <!-- <div id="LoginForm">
-      <label>UserName:</label>
-      <input type="text" id="usernameInput" size="15"/>
-      <label>Password:</label>
-      <input type="text" id="passwordInput" size="15"/>
-      <br>
-      <input type="button" id="loginbutton" value="Login"/><br>
-      <a href='signup.php' style="text-decoration: none;"> <input type="button" id='signupButton' value="Sign up"/> </a>
-      <br>
-      <label id="loginstatus" style="color:Red; display:none" >UserName/Password combination is not correct</label>
+    <div>
+        <label>Hello <?php echo $username?></label>
     </div>
-    <label id="UserName" for="displayUserName" style="display:none"></label> -->
     <br>
     <div>
         <form method="post" action="friend_request.php">
@@ -73,13 +69,6 @@
 
 
     <script>
-      var user = {
-        id : null,
-        name : null,
-        latlng : null,
-        time : null,
-        state : null,
-      }
 
       var filter = {
         id : null,
@@ -92,6 +81,7 @@
       var infoWindow;
       var locationSelect;
       var usermarker = null;
+      var bounds;
 
 
         function initMap() {
@@ -103,7 +93,7 @@
             mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
           });
           infoWindow = new google.maps.InfoWindow();
-
+          bounds = new google.maps.LatLngBounds();
           usermarker = new google.maps.Marker({ //Current Location marker
                           map: map,
                           animation: google.maps.Animation.DROP,
@@ -111,7 +101,6 @@
                           draggable:true
                         }); //change marker location by using usermarker.setPosition(latlng)
 
-          loginButton = document.getElementById("loginbutton").onclick = login;
 
           map.addListener('click',function(event) {
             if(usermarker.getPosition() != null){
@@ -126,6 +115,14 @@
             UpdateMapMarkerView();
           });
 
+          var latlng = new google.maps.LatLng(<?php echo $ulat;?>, <?php echo $ulng;?>);
+
+          usermarker.setPosition(latlng);
+          map.setCenter(latlng);
+          bounds.extend(latlng);
+          map.fitBounds(bounds);
+
+          GetFilters();
         }
 
 
@@ -136,60 +133,41 @@
             document.getElementById('lng').value = event.latLng.lng();
         }
 
-        function login(){
-          var status = document.getElementById("loginstatus");
-          status.style.display = "none";
-          var username = document.getElementById("usernameInput").value;
-          var psd = document.getElementById("passwordInput").value;
-          loginRequest("login.php?username="+username+"&password="+psd, function(data){
-                var xml = parseXml(data);
-                var userNodes = xml.documentElement.getElementsByTagName("users");
-                var id = userNodes[0].getAttribute("id");
-                var name = userNodes[0].getAttribute("username");
-                var latlng = new google.maps.LatLng(
-                  parseFloat(userNodes[0].getAttribute("lat")),
-                  parseFloat(userNodes[0].getAttribute("lng")));
-                var time = userNodes[0].getAttribute("time");
-                var state = userNodes[0].getAttribute("state");
-                user.id = id;
-                user.name = name;
-                user.latlng = latlng;
-                user.time = time;
-                user.state = state;
-                document.getElementById("UserName").textContent = "Hello " + name;
-                document.getElementById("UserName").style.display = "inline";
-                document.getElementById("LoginForm").style.display = "none";
-                map.setCenter(latlng);
-                usermarker.setPosition(latlng);
-
-                GetFilters();
-
-
-                UpdateMapMarkerView();
-        });
+      function UpdateMapMarkerView(){ //Update the Note Markers everytime we change our time/location or even login since we display all notes on the map
+            clearMarkers();
+            var url = "GetNotes.php?uid=" + <?php echo $uid ?>;
+            downloadUrl(url, function(data){
+              var xml = parseXml(data);
+              var NotesNodes = xml.documentElement.getElementsByTagName("Note");
+              
+              for(var i = 0; i < NotesNodes.length; i++){
+                  var nid = NotesNodes[i].getAttribute("nid");
+                  var place_name = NotesNodes[i].getAttribute("place_name");
+                  var address = NotesNodes[i].getAttribute("address");
+                  var ncontent = NotesNodes[i].getAttribute("ncontent");
+                  var latlng = new google.maps.LatLng(
+                  parseFloat(NotesNodes[i].getAttribute("lat")),
+                  parseFloat(NotesNodes[i].getAttribute("lng")));
+                  createMarker(latlng, place_name, ncontent);
+                  bounds.extend(latlng);
+              }
+              map.fitBounds(bounds)
+            });
       }
 
-      function UpdateMapMarkerView(){ //Update the Note Markers everytime we change our time/location or even login since we display all notes on the map
-            // var url = "GetNotes.php?uid=" + user.id;
-            // downloadUrl(url, function(data)){
-            //   var xml = parseXml(data);
-            //   var NotesNodes = xml.documentElement.getElementByTagName("Note");
-            //   for(var i = 0; i < NotesNodes.length; i++){
-            //       //need note id
-            //       //need note location's name
-            //   }
-
-            //   createMarker(latlng, name, address);
-            //   bounds.extend(latlng);
-            //   map.setCenter(latlng);
-            // }
-            // map.fitBounds(bounds);
+      function clearMarkers(){
+        if(infoWindow){
+          infoWindow.close();
+        }
+        for (var i = 0; i < markers.length; i++) {
+           markers[i].setMap(null);
+         }
+         markers.length = 0;
       }
 
         function GetFilters(){
-          if(user.id != null){
-            url = "getAllFilters.php?uid=" + user.id;
-            GetFiltersRequest(url, function(data){
+            url = "getAllFilters.php?uid=" + <?php echo $uid?>;
+            downloadUrl(url, function(data){
               var xml = parseXml(data);
               var filterNodes = xml.documentElement.getElementsByTagName("filter");
               var table = document.getElementById("FilterTable");
@@ -206,107 +184,12 @@
                   cell3.innerHTML = "<a href=deleteFilter.php?id=" + id + ">Delete this Filter</a>";
               }
             });
-          }
         }
-
-        function GetFiltersRequest(url, callback){
-          var request = window.ActiveXObject ?
-              new ActiveXObject('Microsoft.XMLHTTP') :
-              new XMLHttpRequest;
-
-          request.onreadystatechange = function() {
-            if (request.readyState == 4) {
-              request.onreadystatechange = doNothing;
-              callback(request.responseText, request.status);
-            }
-          };
-
-          request.open('GET', url, true);
-          request.send(null);
-        }
-
-
-        function loginRequest(url, callback){
-          var request = window.ActiveXObject ?
-              new ActiveXObject('Microsoft.XMLHTTP') :
-              new XMLHttpRequest;
-
-           request.onreadystatechange = function() {
-            if (request.readyState == 4) {
-              request.onreadystatechange = doNothing;
-              callback(request.responseText, request.status);
-            }
-            else{
-              var status = document.getElementById("loginstatus");
-              status.style.display = "inline";
-            }
-          };
-
-          request.open('GET', url, true);
-          request.send(null);
-        }
-
-
-       function searchLocations() {
-         var address = document.getElementById("addressInput").value;
-         var geocoder = new google.maps.Geocoder();
-         geocoder.geocode({address: address}, function(results, status) {
-           if (status == google.maps.GeocoderStatus.OK) {
-            searchLocationsNear(results[0].geometry.location);
-           } else {
-             alert(address + ' not found');
-           }
-         });
-       }
-
-       function clearLocations() {
-         infoWindow.close();
-         for (var i = 0; i < markers.length; i++) {
-           markers[i].setMap(null);
-         }
-         markers.length = 0;
-
-         locationSelect.innerHTML = "";
-         var option = document.createElement("option");
-         option.value = "none";
-         option.innerHTML = "See all results:";
-         locationSelect.appendChild(option);
-       }
-
-       function searchLocationsNear(center) {
-         clearLocations();
-
-         var radius = document.getElementById('radiusSelect').value;
-         var searchUrl = 'locations.php?lat=' + center.lat() + '&lng=' + center.lng() + '&radius=' + radius;
-         var tmp = 1;
-         downloadUrl(searchUrl, function(data) {
-           var xml = parseXml(data);
-           var markerNodes = xml.documentElement.getElementsByTagName("marker");
-           var bounds = new google.maps.LatLngBounds();
-           for (var i = 0; i < markerNodes.length; i++) {
-             var id = markerNodes[i].getAttribute("id");
-             var name = markerNodes[i].getAttribute("name");
-             var address = markerNodes[i].getAttribute("address");
-             var distance = parseFloat(markerNodes[i].getAttribute("distance"));
-             var latlng = new google.maps.LatLng(
-                  parseFloat(markerNodes[i].getAttribute("lat")),
-                  parseFloat(markerNodes[i].getAttribute("lng")));
-
-             createOption(name, distance, i);
-
-           }
-
-           locationSelect.style.visibility = "visible";
-           locationSelect.onchange = function() {
-             var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
-             google.maps.event.trigger(markers[markerNum], 'click');
-           };
-         });
-       }
 
        function createMarker(latlng, name, address) {
           var html = "<b>" + name + "</b> <br/>" + address;
           var marker = new google.maps.Marker({
+            animation: google.maps.Animation.DROP,
             map: map,
             position: latlng
           });
@@ -316,13 +199,6 @@
           });
           markers.push(marker);
         }
-
-       function createOption(name, distance, num) {
-          var option = document.createElement("option");
-          option.value = num;
-          option.innerHTML = name;
-          locationSelect.appendChild(option);
-       }
 
        function downloadUrl(url, callback) {
           var request = window.ActiveXObject ?
@@ -352,11 +228,14 @@
 
        function doNothing() {}
   </script>
+  
   <script type='text/javascript' src='config.js' ></script>
   <script>
   var my_key = config.MY_KEY;
   document.write('<script async defer src="https://maps.googleapis.com/maps/api/js?key=' + my_key + '&callback=initMap"><' + '/script>');
   </script>
-
+  <script >
+        UpdateMapMarkerView();
+  </script>
   </body>
 </html>

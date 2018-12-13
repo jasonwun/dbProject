bf50b5556002daf09e5fba3a1b87b6c15acc05f5
JUNDA YIN <yinjunda.cool@gmail.com>
2018-12-13 11:36:28 -0500

    Update Session data when we upload data to database

--- UpdateUser.php
+++ UpdateUser.php
@@ -8,6 +8,10 @@ $lat = $_GET["lat"];
 $lng = $_GET["lng"];
 
 
+$_SESSION["ulat"] = $lat;
+$_SESSION["ulng"] = $lng;
+
+
 $query = sprintf("update users set ulatt = %.7lf, ulong = %.7lf where uid = %d", $lat, $lng, $uid);
 $pdo->query($query);
 
